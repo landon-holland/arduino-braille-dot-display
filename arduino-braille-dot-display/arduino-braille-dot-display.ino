41266983616ab60adf184beccf61fa60c95c092f
Landon Holland <landon.holland02@gmail.com>
2021-10-23 12:58:43 -0400

    Add support for NVDA as braille display and add beginnings of button input.

--- arduino-braille-dot-display/arduino-braille-dot-display.ino
+++ arduino-braille-dot-display/arduino-braille-dot-display.ino
@@ -9,16 +9,38 @@
 #define MAX_DEVICES 4
 #define CS_PIN 3
 
+// Define pins for 8 button braille keyboard.
+#define BUTTON1_PIN 2
+#define BUTTON2_PIN 5
+#define BUTTON3_PIN 6
+#define BUTTON4_PIN 7
+#define BUTTON5_PIN 8
+#define BUTTON6_PIN 9
+#define BUTTON7_PIN 10
+#define BUTTON8_PIN 4
+
 // Object for dot display
 MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
 // 2D Array for Braille Dot Display
 // 0 - Off
 // 1 - On
+// Binary representation of braille:
+// 2 3
+// 1 4
+// 0 5
 uint8_t braille[127];
 
 // Current character position for Braille.
 uint8_t current_pos = 0;
+
+// Current mode
+// 0 - Arduino standalone input mode
+// 1 - Arduino NVDA driver mode
+uint8_t mode = 0;
+
+// 32 byte buffer
+byte buf[32];
 
 // Initialize braille array with correct binary representations of braille.
 void init_braille_array() {
@@ -90,6 +112,7 @@ void init_braille_array() {
   braille[95] = 0b000111;
 }
 
+// Output braille character to dot matrix.
 void output_braille(char c) {
   uint8_t braille_char = braille[c];
   uint8_t current_x;
@@ -115,6 +138,37 @@ void output_braille(char c) {
   current_pos++;
 }
 
+// Output character to dot matrix using nvda style binary representation of braille.
+void output_braille_nvda(uint8_t c) {
+  uint8_t current_x;
+  uint8_t x_reset;
+  uint8_t current_y = (2 * current_pos) % 32;
+  uint8_t x_count = 0;
+  uint8_t eight_dot_count = 7;
+  if (current_pos < 16) {
+    x_reset = 7;
+  } else {
+    x_reset = 3;
+  }
+  current_x = x_reset;
+  for (uint8_t i = 0; i < 6; i++) {
+    mx.setPoint(current_x, current_y, bitRead(c, i));
+    x_count++;
+    if (x_count == 3) {
+      current_x = x_reset;
+      current_y++;
+    } else {
+      current_x--;
+    }
+  }
+  // account for 7 and 8 bit now
+  current_y = (2 * current_pos) % 32;
+  mx.setPoint(x_reset - 3, current_y, bitRead(c, 6));
+  mx.setPoint(x_reset - 3, current_y + 1, bitRead(c, 7));
+  current_pos++;
+}
+
+// Print given string in braille.
 void print_braille_str(const char *str) {
   uint8_t i = 0;
   while (str[i] != 0) {
@@ -124,14 +178,47 @@ void print_braille_str(const char *str) {
 }
 
 void setup() {
+  // Initialize Serial bus.
+  Serial.begin(19200);
+  Serial.setTimeout(1);
   // Initialize the braille array.
-  Serial.begin(9600);
   init_braille_array();
+  // Initialize pins for buttons.
+  pinMode(BUTTON1_PIN, INPUT);
+  pinMode(BUTTON2_PIN, INPUT);
+  pinMode(BUTTON3_PIN, INPUT);
+  pinMode(BUTTON4_PIN, INPUT);
+  pinMode(BUTTON5_PIN, INPUT);
+  pinMode(BUTTON6_PIN, INPUT);
+  pinMode(BUTTON7_PIN, INPUT);
+  pinMode(BUTTON8_PIN, INPUT);
   // Start dot display.
   mx.begin();
-  print_braille_str("TEST");
 }
 
 void loop() {
-
+  // Check mode first
+  if (mode == 1) {
+    if (Serial.available() >= 32) {
+      // Output braille binary if 32 bytes found.
+      current_pos = 0;
+      size_t len = Serial.readBytes(buf, 32);
+      for (int i = 0; i < len; i++) {
+        output_braille_nvda(buf[i]);
+      }
+    }
+  } else {
+    int buttonState1 = digitalRead(BUTTON1_PIN);
+    int buttonState2 = digitalRead(BUTTON2_PIN);
+    if (buttonState1 == HIGH) {
+      mx.setPoint(0, 0, true);
+    } else {
+      mx.setPoint(0, 0, false);
+    }
+    if (buttonState2 == HIGH) {
+      mx.setPoint(1, 0, true);
+    } else {
+      mx.setPoint(1, 0, false);
+    }
+  }
 }
